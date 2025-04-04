a2758e2750f0e1bf7043966479d5e86714c67121
joegamal <yusufmagamalth@gmail.com>
2025-04-05 00:11:10 +0200

    hibernate

--- databaseWithHibernate/dbHibernate/src/main/java/com/ygCodeCorner/App.java
+++ databaseWithHibernate/dbHibernate/src/main/java/com/ygCodeCorner/App.java
@@ -1,13 +1,7 @@
 package com.ygCodeCorner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
